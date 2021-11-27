a1eb19f184f38a5d2840fa93ebdc1e58c5b35ca9
Bigweld <therealbigweld@pm.me>
2021-11-26 23:10:50 -0500

    Move loose variables to "stats" dictionary

--- main.py
+++ main.py
@@ -29,21 +29,17 @@ print('Done')
 stats={
     "good": 0,
     "bad": 0,
-    "lobbies": 0
+    "lobbies": 0,
+    "checked": 0,
+    "valid": []
 }
 
-# Initialize list for valid codes, and counter for checked codes
-goodlist=[]
-checked  = 0
-
 def checkcode(code):
-    global goodlist
-    global checked
     global stats
 
     # Check code
     r = requests.get(f'https://ecast.jackboxgames.com/api/v2/rooms/{code}')
-    checked += 1
+    stats["checked"] += 1
 
     if r.status_code == 404:
         # Invalid
@@ -58,11 +54,11 @@ def checkcode(code):
             if r.json()["body"]["audienceEnabled"] == False:
                 # Lobby game
                 stats["lobbies"] += 1
-                goodlist.append(code) # Write code to list
+                stats["valid"].append(code) # Write code to list
 
         else:
             # User wants all found codes, write code to list
-            goodlist.append(code)
+            stats["valid"].append(code)
 
 
 processes = []
@@ -74,11 +70,11 @@ with ThreadPoolExecutor(max_workers=500) as executor:
         # Loop through all codes
         processes.append(executor.submit(checkcode, code))
         # Update user with stats and code list
-        print(f'All Valid: {stats["good"]}, Lobbies: {stats["lobbies"]}, Checked: {checked}\n\n\nCodes:\n{goodlist}\n\n')
+        print(f'All Valid: {stats["good"]}, Lobbies: {stats["lobbies"]}, Checked: {stats["checked"]}\n\n\nCodes:\n{stats["valid"]}\n\n')
 
 # Write final stats and write to file
-print(f'Bad codes: {stats["bad"]}, Good codes: {stats["good"]}, Checked total: {checked}')
+print(f'Bad codes: {stats["bad"]}, Good codes: {stats["good"]}, Checked total: {stats["checked"]}')
 with open('valid.txt', 'w') as f:
-    for code in goodlist:
+    for code in stats["valid"]:
         f.writeline(code)
 print("Wrote all codes to a file!")
